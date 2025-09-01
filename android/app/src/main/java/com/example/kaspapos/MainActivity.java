57391767f1ef0d2bf2aa6c270f451f50dd17e757
Aniket Thakur <jubi.aniket@gmail.com>
2025-09-01 11:50:14 +0000

    The `npm install` command failed in my project. Analyze the following er

--- android/app/src/main/java/com/example/kaspapos/MainActivity.java
+++ android/app/src/main/java/com/example/kaspapos/MainActivity.java
@@ -1,5 +1,20 @@
 package com.example.kaspapos;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
+import com.getcapacitor.community.share.Share;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      add(Share.class);
+    }});
+  }
+}
